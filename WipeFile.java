11941a0ac541da0d106015efa40c81c6b8c35a01
TheLazyGamer <busalambo@gmail.com>
2019-09-14 00:04:44 -0700

    Delete WipeFile.java

--- WipeFile.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
- 
-public class WipeFile {
-    public static void main(String[] argv) throws FileNotFoundException {
-        PrintWriter pw = new PrintWriter("filepath.txt");
-        pw.close();
-    }
-}

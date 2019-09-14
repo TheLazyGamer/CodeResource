86f7947a1bdf63f7169fc09860b0edea7840b795
TheLazyGamer <busalambo@gmail.com>
2019-09-14 00:30:32 -0700

    Added NotResponding

---
+++ NotResponding.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NotResponding {
+	public static void main(String[] argv) throws IOException {
+		Process respondingCmd = Runtime.getRuntime().exec("tasklist /FI \"STATUS eq not responding\"");
+
+		BufferedReader programsResponding = new BufferedReader(new InputStreamReader(respondingCmd.getInputStream()));
+		String respondingLog = "";
+
+		while ((respondingLog = programsResponding.readLine()) != null) {
+			respondingLog = respondingLog.trim();
+			System.out.println("Found2 " + respondingLog);
+		}
+
+		programsResponding.close();
+	}
+}

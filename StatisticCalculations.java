a9332ef315bbeb6c4e331ab8f821d167012788e5
TheLazyGamer <busalambo@gmail.com>
2020-09-20 16:47:56 -0700

    Added StandardDeviation

--- StatisticCalculations.java
+++ StatisticCalculations.java
@@ -2,8 +2,29 @@ import java.util.ArrayList;
 
 public class StatisticCalculations {
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
+		double[] numArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		double SD = calculateSD(numArray);
 
+		System.out.format("Standard Deviation = %.6f", SD);
+	}
+
+	public static double calculateSD(double numArray[])
+	{
+		double sum = 0.0, standardDeviation = 0.0;
+		int length = numArray.length;
+
+		for(double num : numArray) {
+			sum += num;
+		}
+
+		double mean = sum/length;
+
+		for(double num: numArray) {
+			standardDeviation += Math.pow(num - mean, 2);
+		}
+
+		return Math.sqrt(standardDeviation/length);
 	}
 
 	/**
@@ -84,7 +105,4 @@ public class StatisticCalculations {
 		int index = (int) Math.ceil(percentile / 100.0 * responseTimes.size());
 		return responseTimes.get(index-1);
 	}
-
-
-
 }
